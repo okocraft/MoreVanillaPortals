34da540babfab898c0134b4be7b273d70bdd0f68
Siroshun09 <siro.shun8@gmail.com>
2021-10-25 11:01:54 +0900

    improve: use EntityInsideBlockEvent

--- src/main/java/net/okocraft/morevanillaportals/listener/EndPortalListener.java
+++ src/main/java/net/okocraft/morevanillaportals/listener/EndPortalListener.java
@@ -1,5 +1,6 @@
 package net.okocraft.morevanillaportals.listener;
 
+import io.papermc.paper.event.entity.EntityInsideBlockEvent;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.okocraft.morevanillaportals.util.WorldNameMap;
@@ -8,40 +9,41 @@ import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPortalEnterEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class EndPortalListener implements Listener {
 
-    private final WorldNameMap worldNameMap = WorldNameMap.end();
+    private final WorldNameMap endMap = WorldNameMap.end();
 
-    @EventHandler
-    public void onEnter(@NotNull EntityPortalEnterEvent event) {
-        if (!(event.getEntity() instanceof CraftPlayer player) ||
-                !(player.getWorld() instanceof CraftWorld world)) {
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onEvent(@NotNull EntityInsideBlockEvent event) {
+        if (event.isCancelled() ||
+                !(event.getEntity() instanceof CraftPlayer player) ||
+                !(event.getBlock().getWorld() instanceof CraftWorld world)
+        ) {
             return;
         }
 
-        if (world.getBlockAt(event.getLocation()).getType() != Material.END_PORTAL) {
-            return;
+        if (event.getBlock().getType() == Material.END_PORTAL) {
+            var resourceKey = world.getHandle().getTypeKey() == DimensionType.END_LOCATION ? Level.OVERWORLD : Level.END;
+            var server = world.getHandle().getServer();
+
+            if (Bukkit.getAllowEnd() && server.getLevel(resourceKey) != null) {
+                return;
+            }
+
+            var destinationWorldName = endMap.getWorldNameOfPortalDestination(world);
+
+            if (destinationWorldName == null || destinationWorldName.isEmpty() ||
+                    !(Bukkit.getWorld(destinationWorldName) instanceof CraftWorld destination)) {
+                return;
+            }
+
+            player.getHandle().changeDimension(destination.getHandle(), PlayerTeleportEvent.TeleportCause.END_PORTAL);
+            event.setCancelled(true);
         }
-
-        var resourceKey = world.getHandle().getTypeKey() == DimensionType.END_LOCATION ? Level.OVERWORLD : Level.END;
-        var server = world.getHandle().getServer();
-
-        if (Bukkit.getAllowEnd() && server.getLevel(resourceKey) != null) {
-            return;
-        }
-
-        var destinationWorldName = worldNameMap.getWorldNameOfPortalDestination(world);
-
-        if (destinationWorldName == null || destinationWorldName.isEmpty() ||
-                !(Bukkit.getWorld(destinationWorldName) instanceof CraftWorld destination)) {
-            return;
-        }
-
-        player.getHandle().changeDimension(destination.getHandle(), PlayerTeleportEvent.TeleportCause.END_PORTAL);
     }
 }
