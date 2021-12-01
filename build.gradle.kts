93840f5ccefedf367b3c953feeb6fc3dacbdf483
Siroshun09 <siro.shun8@gmail.com>
2021-12-02 00:51:36 +0900

    build: update userdev, and target mc 1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.1.14"
+    id("io.papermc.paperweight.userdev") version "1.3.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.0"
 }
 
 group = "net.okocraft.morevanillaportals"
 version = "1.1"
 
-val mcVersion = "1.17.1"
+val mcVersion = "1.18"
 val fullVersion = "${version}-mc${mcVersion}"
 
 repositories {
@@ -20,8 +20,8 @@ dependencies {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
@@ -38,7 +38,7 @@ tasks {
 
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(16)
+        options.release.set(17)
     }
 
     processResources {
@@ -50,6 +50,6 @@ bukkit {
     name = "MoreVanillaPortals"
     main = "net.okocraft.morevanillaportals.MoreVanillaPortalsPlugin"
     version = fullVersion
-    apiVersion = "1.17"
+    apiVersion = "1.18"
     author = "Siroshun09"
 }
