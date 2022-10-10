b00920948a9bf3034dc21e4405b0fe1e9ce6122a
Siroshun09 <siro.shun8@gmail.com>
2022-10-10 13:21:19 +0900

    feat: entity portal handling

--- src/main/java/net/okocraft/morevanillaportals/listener/PortalListener.java
+++ src/main/java/net/okocraft/morevanillaportals/listener/PortalListener.java
@@ -1,15 +1,16 @@
 package net.okocraft.morevanillaportals.listener;
 
 import io.papermc.paper.event.entity.EntityInsideBlockEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.okocraft.morevanillaportals.util.WorldNameMap;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_19_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_19_R1.block.CraftBlock;
-import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -35,41 +36,38 @@ public class PortalListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    public void handleInsideBlockEvent(@NotNull EntityInsideBlockEvent event) {
-        var entity = event.getEntity();
-
-        // org.bukkit.entity.Entity#isEmpty = Entity#getPassengers.isEmpty
-        if (entity.isDead() || !entity.isValid() ||
-                entity.getVehicle() != null || !entity.isEmpty() ||
-                !(entity instanceof CraftPlayer player)) {
+    public void handleInsideEndPortal(@NotNull EntityInsideBlockEvent event) {
+        if (event.getBlock().getType() != Material.END_PORTAL ||
+                event.getEntity().getWorld().getEnvironment() != World.Environment.NORMAL ||
+                !(event.getBlock() instanceof CraftBlock block) ||
+                !(event.getEntity() instanceof CraftEntity craftEntity) ||
+                !(event.getEntity().getWorld() instanceof CraftWorld world) // EndPortalBlock#entityInside - CraftWorld#getHandle -> ServerLevel
+        ) {
             return;
         }
 
-        if (!isPortal(event.getBlock()) || !(event.getBlock() instanceof CraftBlock block)) {
-            return;
-        }
+        // EndPortalBlock#entityInside
+        var entity = craftEntity.getHandle();
+        var pos = block.getPosition();
+        if (!entity.isPassenger() &&
+                !entity.isVehicle() &&
+                entity.canChangeDimensions() &&
+                Shapes.joinIsNotEmpty(Shapes.create(entity.getBoundingBox().move((double) (-pos.getX()), (double) (-pos.getY()), (double) (-pos.getZ()))), block.getNMS().getShape(world.getHandle(), pos), BooleanOp.AND)) {
+            var destinationWorldName = endMap.getWorldNameOfPortalDestination(world);
 
-        player.getHandle().handleInsidePortal(block.getPosition());
-
-        if (player.getHandle().isInsidePortal) {
-            player.getHandle().isInsidePortal = false;
-        } else {
-            return;
-        }
-
-        var world = block.getWorld();
-
-        if (block.getType() == Material.NETHER_PORTAL && isOverworldOrNether(world.getEnvironment())) {
-            if (handleNetherPortal(player)) {
-                event.setCancelled(true);
+            if (destinationWorldName == null || destinationWorldName.isEmpty() ||
+                    !(Bukkit.getWorld(destinationWorldName) instanceof CraftWorld destination)) {
+                return;
             }
-            return;
-        }
 
-        if (block.getType() == Material.END_PORTAL && isOverworldOrEnd(world.getEnvironment())) {
-            if (handleEndPortal(player)) {
-                event.setCancelled(true);
+            if (entity instanceof ServerPlayer player) {
+                player.changeDimension(destination.getHandle(), PlayerTeleportEvent.TeleportCause.END_PORTAL);
+            } else {
+                //noinspection ConstantConditions
+                entity.teleportTo(destination.getHandle(), null);
             }
+
+            event.setCancelled(true);
         }
     }
 
