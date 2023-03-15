eaa17d0ed75117812d80974ed40dd685efecaad0
Siroshun09 <siro.shun8@gmail.com>
2023-03-16 04:23:26 +0900

    build: update to Minecraft 1.19.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "net.okocraft.morevanillaportals"
 version = "1.5"
 
-val mcVersion = "1.19.3"
+val mcVersion = "1.19.4"
 val fullVersion = "${version}-mc${mcVersion}"
 
 repositories {
