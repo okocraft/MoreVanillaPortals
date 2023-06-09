990338c56c44989ab3837e9134c06c38311c1783
Siroshun09 <siro.shun8@gmail.com>
2023-06-09 20:00:12 +0900

    build: remove plugin.yml generator plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     `java-library`
     id("io.papermc.paperweight.userdev") version "1.5.5"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
 }
 
 group = "net.okocraft.morevanillaportals"
-version = "1.6"
+version = "1.7"
 
 val mcVersion = "1.20"
 val fullVersion = "${version}-mc${mcVersion}"
@@ -42,13 +41,8 @@ tasks {
 
     processResources {
         filteringCharset = Charsets.UTF_8.name()
+        filesMatching("plugin.yml") {
+            expand("projectVersion" to version)
+        }
     }
-}
-
-bukkit {
-    name = "MoreVanillaPortals"
-    main = "net.okocraft.morevanillaportals.MoreVanillaPortalsPlugin"
-    version = fullVersion
-    apiVersion = "1.20"
-    author = "Siroshun09"
 }
